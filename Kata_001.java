a721dd8eaeea2480c1a1fec0290e325221e231fc
sam98963 <szwilliamson01@gmail.com>
2023-07-26 17:51:29 +0100

    first java kata

---
+++ Kata_001.java
@@ -0,0 +1,15 @@
+package Java_Fundamentals_Training;
+
+public class Kata_001 {
+  public static int sumMul(int n, int m) {
+      if (n <= 0 || m < 0) {
+          throw new IllegalArgumentException("Both n and m should be positive integers.");
+      }
+
+      int sum = 0;
+      for (int i = n; i < m; i += n) {
+          sum += i;
+      }
+      return sum;
+  }
+}
---
+++ java_katas_tracked.md
@@ -0,0 +1,5 @@
+### Kata_001 ###
+8 kyu
+Sum of Multiples
+26/07/23
+I learnt about IllegalArgumentException - to handle invalid arguements passed to a method. throw new IllegalArgumentException(s:"enter descriptive string here")
